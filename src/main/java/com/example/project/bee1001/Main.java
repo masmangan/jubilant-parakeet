bc77d124a5d3176737c1a68f0c195c31be4860c8
Renato Souza <97472730+Renatu5@users.noreply.github.com>
2025-08-15 01:11:41 +0000

    atualizando Main.java - @Renatu5

--- src/main/java/com/example/project/bee1001/Main.java
+++ src/main/java/com/example/project/bee1001/Main.java
@@ -1,13 +1,10 @@
 package com.example.project.bee1001;
 
 import java.io.IOException;
- 
 
 public class Main {
  
     public static void main(String[] args) throws IOException {
- 
- 
     }
  
 }
